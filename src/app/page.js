ac1ae38669a8a6315580ddd0a6700d9b04b79cfc
CodeWithYash2412 <84624661+CodeWithYash2412@users.noreply.github.com>
2024-07-02 17:20:13 +0530

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,104 @@
-import Image from "next/image";
+"use client"
 
-export default function Home() {
+import { useEffect, useState } from "react";
+import Skeleton from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css';
+
+const Page = () => {
+  const [users, setUsers] = useState([]);
+  const [selectedUser, setSelectedUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetch("https://602e7c2c4410730017c50b9d.mockapi.io/users");
+      const structuredData = await data.json();
+      setUsers(structuredData);
+    };
+
+    fetchData();
+
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 5000); // 5 seconds
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  const handleUserClick = (user) => {
+    setSelectedUser(user);
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <div className="flex h-screen">
+      <div className="w-1/3 border-r border-gray-300 p-4 overflow-y-auto">
+        {loading ? (
+          <div>
+            {[...Array(10)].map((_, index) => (
+              <div key={index} className="mb-2 flex items-center">
+                <Skeleton circle={true} height={48} width={48} className="mr-4" />
+                <div>
+                  <Skeleton height={20} width={100} />
+                  <Skeleton height={15} width={150} />
+                </div>
+              </div>
+            ))}
+          </div>
+        ) : (
+          users.map((user) => (
+            <div
+              key={user.id}
+              onClick={() => handleUserClick(user)}
+              className={`cursor-pointer p-2 hover:bg-gray-200 hover:text-black rounded mb-2 flex items-center ${selectedUser ? user.profile.username === selectedUser.profile.username ? 'bg-cyan-400 text-white' : '' : ''}`}
+            >
+              <img
+                src={user.avatar}
+                alt={`${user.profile.username}'s avatar`}
+                className="w-12 h-12 rounded-full mr-4"
+              />
+              <div>
+                <h3 className="text-lg font-semibold">{user.profile.firstName} {user.profile.lastName}</h3>
+                <p>{user.profile.username}</p>
+              </div>
+            </div>
+          ))
+        )}
+      </div>
+      <div className="w-2/3 p-4 overflow-y-auto flex justify-center items-center">
+        {selectedUser ? (
+          <div className="flex items-center flex-col justify-center h-2/3 bg-slate-300 w-2/3 p-10 rounded-xl shadow-xl">
+            <img
+              src={selectedUser.avatar}
+              alt={`${selectedUser.profile.username}'s avatar`}
+              className="w-1/4 rounded-full"
             />
-          </a>
-        </div>
+            <p className="mt-4 text-xl font-bold">
+              {selectedUser.profile.firstName} {selectedUser.profile.lastName}
+            </p>
+            <p className="mt-2 text-gray-900 text-center">{selectedUser.profile.username}</p>
+            <p className="mt-2 text-gray-900 text-center">{selectedUser.profile.email}</p>
+            <p className="mt-2 text-gray-900 text-center">{selectedUser.Bio}</p>
+            <p className="mt-2 text-gray-700 text-center bg-cyan-300 p-2 rounded-xl font-mono">
+              {selectedUser.jobTitle}
+            </p>
+          </div>
+        ) : loading ? (
+          <div className="flex items-center flex-col justify-center h-full">
+            <Skeleton circle={true} height={120} width={120} className="mb-4" />
+            <Skeleton height={20} width={200} className="mb-2" />
+            <Skeleton height={15} width={150} className="mb-2" />
+            <Skeleton height={15} width={250} className="mb-2" />
+            <Skeleton height={15} width={300} className="mb-2" />
+          </div>
+        ) : (
+          <p className="text-gray-500">Select a user to see their details</p>
+        )}
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   );
-}
+};
+
+export default Page;
+
+
+
